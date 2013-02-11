162185b3bebbe13c32c08d1b1404a9a7cbc10ba3
matt harrison <matthewharrison@gmail.com>
2013-02-11 13:19:52 -0700

    Update tango-theme.el
    remove conflict with paren highlighting

--- tango-theme.el
+++ tango-theme.el
@@ -94,7 +94,7 @@
      (mumamo-background-chunk-submode ((t (:background ,alum5))))
      (bold ((t (:bold t))))
      (highlight ((t (:foreground ,alum1 :bold t))))
-     (hl-line ((t (:background ,alum5 :bold t))))
+     (hl-line ((t (:background ,plum3 :bold t))))
      (match ((t (:foreground ,cham2 :bold t))))
      (link ((t (:foreground ,cham1))))
      ;;(link-visited ((t (:foreground ,plum2))))
